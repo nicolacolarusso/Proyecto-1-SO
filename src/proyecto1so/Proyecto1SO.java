9b9476e798c5c6872ec65fb7cab4aa101df8bad3
Gabriele Colarusso <131668387+nicolacolarusso@users.noreply.github.com>
2024-10-03 18:51:43 -0400

    first commit2

--- src/proyecto1so/Proyecto1SO.java
+++ src/proyecto1so/Proyecto1SO.java
@@ -14,7 +14,6 @@ public class Proyecto1SO {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
     }
     
 }
